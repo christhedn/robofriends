bcc9142b9689fe034a5324f9587d46b19e626a99
christhedn <christian.deitert@gmail.com>
2020-05-30 04:30:11 +0200

    Updates

---
+++ precache-manifest.1e7735e5181444ecadac6755f2c5cfbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3489f256f2e73b705a0fac9a5adedc88",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "72901011dda9f0775319",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "d593ef898abe1fda8ca9",
+    "url": "/robofriends/static/css/main.2b4dd00a.chunk.css"
+  },
+  {
+    "revision": "72901011dda9f0775319",
+    "url": "/robofriends/static/js/2.e56aa9dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e56aa9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d593ef898abe1fda8ca9",
+    "url": "/robofriends/static/js/main.66582f8b.chunk.js"
+  },
+  {
+    "revision": "3a6cd7bf0ee3b3b38bcf",
+    "url": "/robofriends/static/js/runtime-main.db3a75f1.js"
+  }
+]);
